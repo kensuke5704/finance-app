c15fa7a873c36ea7db6de86083cb708dec47dd31
kensuke5704 <kensuke5704@gmail.com>
2026-06-13 23:52:56 +0900

    Import momentum tab override styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./spacing-balance.css";
 import "./frame-cleanup.css";
 import "./date-summary-fixes.css";
 import "./momentum-selection.css";
+import "./momentum-tabs-override.css";
 import PwaRegister from "../components/PwaRegister";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
