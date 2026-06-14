2a0d9b0beb8d0ac2825f61e6af7e080cd623b431
kensuke5704 <kensuke5704@gmail.com>
2026-06-15 01:54:38 +0900

    Bump cache after final visual corrections

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finance-pwa-v9";
+const CACHE_NAME = "finance-pwa-v10";
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const APP_SHELL = [
   `${BASE_PATH}/`,
