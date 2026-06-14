40ca50774b4558c843e24bce948a6f9b5fcbf055
kensuke5704 <kensuke5704@gmail.com>
2026-06-15 01:18:40 +0900

    Bump PWA cache for Deep Canvas UI

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finance-pwa-v6";
+const CACHE_NAME = "finance-pwa-v7";
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const APP_SHELL = [
   `${BASE_PATH}/`,
