1deef8ca8ee5bc9758862590d1967261289b3c3b
kensuke5704 <kensuke5704@gmail.com>
2026-06-15 00:40:54 +0900

    fix: refresh installed PWA cache after UI update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finance-pwa-v3";
+const CACHE_NAME = "finance-pwa-v4";
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const APP_SHELL = [
   `${BASE_PATH}/`,
