058439902bbffc7eff4f6675b3b221a50ef51a4a
川村健介 <kensuke_kawamura@kawamurakensukenoMacBook-Air.local>
2026-07-11 10:35:41 +0900

    Polish desktop type hierarchy

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finance-pwa-v48";
+const CACHE_NAME = "finance-pwa-v49";
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const APP_SHELL = [
   `${BASE_PATH}/`,
